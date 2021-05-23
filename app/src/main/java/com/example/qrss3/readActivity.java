f836144c0d141f200c6d359ecfe229e78625e6f3
YutaMatsumura1016 <yuta.train1016@gmail.com>
2021-05-23 16:21:06 +0900

    両方動いた

--- app/src/main/java/com/example/qrss3/readActivity.java
+++ app/src/main/java/com/example/qrss3/readActivity.java
@@ -18,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -41,6 +42,7 @@ public class readActivity extends AppCompatActivity {
 
     SoundPool soundPool;
     int soundpi;
+    String idmString;
     static String gate;
     NfcAdapter nfcAdapter;
     private CompoundBarcodeView barcodeView;
@@ -63,12 +65,10 @@ public class readActivity extends AppCompatActivity {
                 .setUsage(AudioAttributes.USAGE_GAME)
                 .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
                 .build();
-
         soundPool = new SoundPool.Builder()
                 .setAudioAttributes(audioAttributes)
                 .setMaxStreams(1)
                 .build();
-
 
         soundpi = soundPool.load(this, R.raw.pi_cut, 1);
         nfcAdapter.enableReaderMode(readActivity.this, new MyReaderCallback(), NfcAdapter.FLAG_READER_NFC_F, null);
@@ -78,16 +78,30 @@ public class readActivity extends AppCompatActivity {
         barcodeReader();
     }
 
-
-    public void barcodeReader() { //読み取り処理を行うメソッド
-        barcodeView.decodeSingle(new BarcodeCallback() { //読み取りを行う
+    //バーコードリーダー
+    public void barcodeReader() {
+        barcodeView.decodeSingle(new BarcodeCallback() {
             @Override
             public void barcodeResult(BarcodeResult result) {
-                String readIDM = result.getText();
-                Log.d("huga", readIDM);
-                sendURL(readIDM); //result.getText()で読み取り結果を取得し、ダイアログのメソッドへ
+                soundPool.play(soundpi, 1.0f, 1.0f, 0, 0, 1);
+                idmString = result.getText();
+                TextView resultText = (TextView) findViewById(R.id.resultText);
+                String resultTextIDM = "idm: " + idmString;
+                resultText.setText(resultTextIDM);
+                WebView myWebView = (WebView) findViewById(R.id.webView1);
+                String sentURL = "https://script.google.com/a/wasedasai.net/macros/s/AKfycbw9BMWL3BLRhB8ZlIs32scTBWceP0TYy28wnWtBD2btOatmNiiw/exec?idm=" + idmString + "&&gate=" + gate;
 
+                //GASに送信、結果が帰ってきたら次の読み込み開始
+
+                myWebView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        makeDialog();
+                    }
+                });
+                myWebView.loadUrl(sentURL);
             }
+
 
             @Override
             public void possibleResultPoints(List resultPoints) {
@@ -95,30 +109,22 @@ public class readActivity extends AppCompatActivity {
         });
     }
 
-    private void sendURL(String idmString){
-        new AlertDialog.Builder(this)
-//              .setMessage(mStr + "でOK?")
-                .setPositiveButton("OK!", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 何かしらの処理
-                        barcodeReader(); //もう一回呼び出すことで擬似的に連続読み取り
-                    }
-                })
-                .setCancelable(false)
-                .show();
+    //何も表示しないダイアログを2秒で消すことでQRコード読み取りシステムを再起動
+    private void makeDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                barcodeReader();
+            }
+        });
 
-                soundPool.play(soundpi, 1.0f, 1.0f, 0, 0, 1);
-                TextView resultText = (TextView)findViewById(R.id.resultText);
-                String resultTextIDM = "idm: " + idmString;
-                resultText.setText(resultTextIDM);
-                WebView myWebView = (WebView) findViewById(R.id.webView1);
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {}
 
-                //アプリ内ブラウザを使用
-                myWebView.setWebViewClient(new WebViewClient());
-                String sentURL = "https://script.google.com/a/wasedasai.net/macros/s/AKfycbw9BMWL3BLRhB8ZlIs32scTBWceP0TYy28wnWtBD2btOatmNiiw/exec?idm=" + idmString + "&&gate=" + gate;
-                myWebView.loadUrl(sentURL);
-                Log.d("hugaaaa", sentURL);
+        dialog.cancel();
     }
 
 
@@ -150,7 +156,7 @@ public class readActivity extends AppCompatActivity {
     }
 
 
-    //NFC読み取り結果
+    //NFC読み取り
     class MyReaderCallback implements NfcAdapter.ReaderCallback {
 
         @RequiresApi(api = Build.VERSION_CODES.N)
@@ -164,9 +170,8 @@ public class readActivity extends AppCompatActivity {
             for (byte d : idm) {
                 sb.append(String.format("%02X", d));
             }
-            String idmString = sb.toString();
+            idmString = sb.toString();
             Log.d("hoge", idmString);
-
 
 
             //音を鳴らしてSSに送る
@@ -176,8 +181,8 @@ public class readActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     setContentView(R.layout.activity_read);
-                    WebView myWebView = (WebView)findViewById(R.id.webView1);
-                    TextView resultText = (TextView)findViewById(R.id.resultText);
+                    WebView myWebView = (WebView) findViewById(R.id.webView1);
+                    TextView resultText = (TextView) findViewById(R.id.resultText);
                     String resultTextIDM = "idm: " + idmString;
                     //アプリ内ブラウザを使用
                     myWebView.setWebViewClient(new WebViewClient());
@@ -186,10 +191,19 @@ public class readActivity extends AppCompatActivity {
                     Log.d("huga", sentURL);
 
                     soundPool.play(soundpi, 1.0f, 1.0f, 0, 0, 1);
+
                     resultText.setText(resultTextIDM);
+
+                    //バーコードリーダーの再起動
+                    barcodeView = (CompoundBarcodeView) findViewById(R.id.barcodeView);
+                    barcodeView.resume();
+                    barcodeReader();
                 }
             });
+
+
         }
     }
+
 
 }
